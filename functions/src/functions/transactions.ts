0b32d6332124c972b955a3ab743445119ed1d9d1
Raffael <raffael.hertel@web.de>
2023-01-08 11:43:19 +0100

    feat: kein plan

--- functions/src/functions/transactions.ts
+++ functions/src/functions/transactions.ts
@@ -3,16 +3,18 @@ import * as functions from "firebase-functions";
 const TRANSACTION_DOC_PATH:string = "transactions/{transactionId}";
 const db:FirebaseFirestore.Firestore = functions.app.admin.firestore();
 
-export const updateCreditorBalance = functions.firestore.document(TRANSACTION_DOC_PATH)
+export const updateBalance = functions.firestore.document(TRANSACTION_DOC_PATH)
     .onCreate(async(snap) => {
         
         const value = snap.data();
-        const debtors = value.debtors;
+        const debtors:string[] = value.debtors;
+        const creditorId:string = value.creditorId;
+        const groupId:string = value.groupId;
         
-        await db.collection('balance').where('groupId','==',value.groupId)
-        .where('userId','==',value.creditorId).get().then( result => {
+        await db.collection('balance').where('userId','==',creditorId)
+        .where('groupId','==',groupId).get().then( result => {
                 result.docs.map( async doc => {
-                    functions.logger.log(doc);
+                    functions.logger.log(doc.data());
                     const oldAmount:number = doc.data().amount;
                     const toPay:number = Math.round(value.amount/debtors.length * 100) / 100 
                     const newAmount:number = oldAmount + value.amount - toPay;
@@ -21,8 +23,8 @@ export const updateCreditorBalance = functions.firestore.document(TRANSACTION_DO
         });
 
         for(let i = 0; i < debtors.length; i++){
-            await db.collection('balance').where('groupId','==',value.groupId)
-            .where('userId','==',debtors[i]).get().then( result => {
+            await db.collection('balance').where('userId','==',debtors[i])
+            .where('groupId','==',groupId).get().then( result => {
                     result.docs.map( async doc => {
                         const oldAmount:number = doc.data().amount;
                         const newAmount:number = oldAmount - value.amount/debtors.length;
