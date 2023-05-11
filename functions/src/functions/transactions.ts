372fa31dea9682ca6dee5b81e61d9251086ba525
Raffael <raffael.hertel@web.de>
2023-05-11 15:15:35 +0200

    clean: transaction.ts

--- functions/src/functions/transactions.ts
+++ functions/src/functions/transactions.ts
@@ -8,13 +8,6 @@ export const updateBalance = functions.firestore.document(TRANSACTION_DOC_PATH)
     .onCreate(async(snap) => {
         
         const value = snap.data();
-<<<<<<< HEAD
-        const debtors = value.debtors;
-        const groupSize = value.debtors.length +1;
-        
-        await db.collection('balance').where('groupId','==',value.groupId)
-        .where('userId','==',value.creditorId).get().then( result => {
-=======
         const debtors:string[] = value.debtors;
         const creditorId:string = value.creditorId;
         const groupId:string = value.groupId;
@@ -22,18 +15,13 @@ export const updateBalance = functions.firestore.document(TRANSACTION_DOC_PATH)
 
         await db.collection('balance').where('userId','==',creditorId)
         .where('groupId','==',groupId).get().then( result => {
->>>>>>> 15fd0d50537c2f22a3f18c38a521bf8e9f36de6b
                 result.docs.map( async doc => {
                     functions.logger.log(doc.data());
                     const oldAmount:number = doc.data().amount;
-<<<<<<< HEAD
-                    const toPay:number = value.amount/groupSize
-=======
                     const toPay:number = Math.round(value.amount/groupSize* 100) / 100 
                     functions.logger.log("GroupSize",groupSize);
                     functions.logger.log("toPay",toPay);
                     functions.logger.log("oldAmount",oldAmount);
->>>>>>> 15fd0d50537c2f22a3f18c38a521bf8e9f36de6b
                     const newAmount:number = oldAmount + value.amount - toPay;
                     const roundedAmount = Math.round(newAmount*100)/100;
                     functions.logger.log("oldAmount: ",oldAmount);
@@ -50,16 +38,9 @@ export const updateBalance = functions.firestore.document(TRANSACTION_DOC_PATH)
             .where('groupId','==',groupId).get().then( result => {
                     result.docs.map( async doc => {
                         const oldAmount:number = doc.data().amount;
-<<<<<<< HEAD
-                        const toPay:number = value.amount/groupSize
-                        const newAmount:number = oldAmount - toPay;
-                        const roundedAmount = Math.round(newAmount*100)/100;
-                   
-                        return doc.ref.update("amount", roundedAmount);
-=======
+
                         const newAmount:number = oldAmount - value.amount/groupSize;
                         return doc.ref.update("amount", newAmount);
->>>>>>> 15fd0d50537c2f22a3f18c38a521bf8e9f36de6b
                     })
             });
         }
